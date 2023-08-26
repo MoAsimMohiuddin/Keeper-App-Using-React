057a870a8e87a0c5a3a885936e181f8f577368d1
Asim Mohiuddin <iamasimmohiuddin@gmail.com>
2023-08-27 01:39:28 +0530

    App.jsx modify

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,17 +5,11 @@ import Note from './Note.jsx';
 import Input from './Input.jsx';
 import notes from './Notes.js';
 
-function createNote(note) {
-    return (
-        <Note key={note.id} title={note.title} content={note.content}/>
-    );
-};
-
 function App() {
     return (
         <div>
             <Header title='Keeper'/>
-            {notes.map(createNote)}
+            {notes.map(note=><Note key={note.id} title={note.title} content={note.content}/>)}
             <Footer/>
         </div>
     )
